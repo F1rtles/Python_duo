c2369be8f3f429f00b68c3cd14645186291c67ef
F1rtles_Win <firtless@gmail.com>
2025-10-12 19:33:42 +0200

    Update lab_math_2.py

--- lab_math_2.py
+++ lab_math_2.py
@@ -24,42 +24,43 @@
 #     else:
 #         y = math.log10(math.exp(x) + 4)
 
-#     print(f"| {x:5.3f} | {y:11.7f}   |")
 #     x += h
-
-
-
-#/
-#/
-#/
-#/
-#/
-#/
-#/
-#/
-
-import math
-
-a = 3.0
-b = 4.0
-h = 0.1
-d = 0.001
-x = a
-
-print("Завдання 2")
-print("  x    |     sum(y)")
-
-while x < b + h / 2:    
-    sum = 0.0
-    k = 1
+#     print(f"| {x:5.3f} | {y:11.7f}   |")
     
-    while True:
-        term =  1 / k * math.tan(x / 2**k)
-        if abs(term) < d:
-            break   
-        k += 1
-        sum += term
 
-    x += h    
-    print(f"x: {x:.1f} | y: {sum:.8f}")
+
+
+#/
+#/
+#/
+#/
+#/
+#/
+#/
+#/
+
+# import math
+
+# a = 3.0
+# b = 4.0
+# h = 0.1
+# d = 0.001
+# x = a
+
+# print("Завдання 2")
+# print("  x    |     sum(y)")
+
+# while x < b + h / 2:    
+#     sum = 0.0
+#     k = 1
+    
+#     while True:
+#         term =  1 / k * math.tan(x / 2**k)
+#         if abs(term) < d:
+#             break   
+#         k += 1
+#         sum += term
+
+#     x += h    
+#     print(f"x: {x:.1f} | y: {sum:.8f}")
     
