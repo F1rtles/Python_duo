7fe22dbbce81cc27de1f3ac248dc8b8ac10501f3
F1rtles_Win <firtless@gmail.com>
2025-10-26 17:50:50 +0200

    Create learn_Py.py

---
+++ learn_Py.py
@@ -0,0 +1,19 @@
+# b = input("Enter a word: ")
+# x = b
+
+# for x in b:
+#   print(x)
+
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+
+# b = "Hello, World!"
+# print(b[:5])
+# print(b[3:8])
+
+# a = "Hello, World!"
+# print(a.replace("l", "J"))
+
+# a = "Hello, World!"
+# print(a.split("l")) 
