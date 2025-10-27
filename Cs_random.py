f11b9f1bee4621278bf248bef88f6095a95968c8
Firtless <tadeivybranets@gmail.com>
2025-10-27 11:00:23 +0200

    Create Cs_random.py

---
+++ Cs_random.py
@@ -0,0 +1,24 @@
+import random
+
+Dust_2 = ["A_site", "B_site", "Mid", "Long", "Short", "Under"]
+
+Mirage = ["A_site", "B_site", "Mid", "Connector", "Palace", "Ramp"]
+
+Inferno = ["A_site", "B_site", "Mid", "Banana", "Apartments"]
+
+Nuke = ["A_site", "B_site", "Mid", "Ramp", "Silo", "Heaven"]
+
+
+while map_result != "exit":
+
+    map_result = input("Enter the map: ")
+
+    if map_result == "Dust_2":
+        play_choice = random.choice(Dust_2)
+    elif map_result == "Mirage":
+        play_choice = random.choice(Mirage)
+    elif map_result == "Inferno":
+        play_choice = random.choice(Inferno)
+    elif map_result == "Nuke":
+        play_choice = random.choice(Nuke)
+    print(f"Random position: {play_choice}")
